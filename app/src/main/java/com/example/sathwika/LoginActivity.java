578e513266b9fa8c1db6f4dbcb023ce0bc60965e
SathwikaNerella <nerellasathwika695@gmail.com>
2025-04-13 15:28:16 +0530

    https://github.com/SathwikaNerella/Findmytutorapp

---
+++ app/src/main/java/com/example/sathwika/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.sathwika;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // your login XML
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent to move to HomeActivity
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish(); // Optional: prevents going back to login screen
+            }
+        });
+    }
+}
